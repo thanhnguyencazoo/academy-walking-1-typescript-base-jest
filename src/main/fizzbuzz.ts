a0c591ce3f43eca335200588a0fafe30fcb8bcc2
thanhnguyencazoo <thanh.nguyen@cazoo.co.uk>
2021-04-30 11:04:14 +0100

    test for when number is multiple of 5

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,12 +1,13 @@
 export const fizzBuzz = (num: number) => {
-  if (num === 3) {
+  if (num === 15) {
+    return "FizzBuzz";
+  }
+  if (num % 3 === 0) {
     return "Fizz";
   }
   if (num === 5) {
     return "Buzz";
   }
-  if (num === 15) {
-    return "FizzBuzz";
-  }
+
   return num.toString();
 };
--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -13,6 +13,10 @@ describe("fizz buzz", () => {
     const number = 5;
     expect(fizzBuzz(number)).toStrictEqual("Buzz");
   });
+  it("it should return buzz when the number is 10", () => {
+    const number = 10;
+    expect(fizzBuzz(number)).toStrictEqual("Buzz");
+  });
   it("it should return the number itself when it is not a multiple of 3", () => {
     const number = 4;
     expect(fizzBuzz(number)).toStrictEqual("4");
