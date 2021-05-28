912aa1e83024d8af2bce64f924c3f0542dc6db87
thanhnguyencazoo <thanh.nguyen@cazoo.co.uk>
2021-05-28 10:18:24 +0100

    implement player O

--- ticTacToe/src/Game.test.ts
+++ ticTacToe/src/Game.test.ts
@@ -1,13 +1,25 @@
 import { Game } from "./Game";
 
-describe('Game', () => {
-  it('player X should go first', () => {
-    const game = new Game()
-    game.play(0,0)
-    expect(game.board).toEqual([
-      ['X', '', ''],
-      ['', '', ''],
-      ['', '', ''],
-    ])
+describe("Game", () => {
+  it("player X should go first", () => {
+    const game = new Game();
+    game.play(0, 0);
+    expect(game.board()).toEqual([
+      ["X", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ]);
+  });
+
+  it("player O should go second", () => {
+    const game = new Game();
+    game.play(0, 0);
+    game.play(0, 1);
+
+    expect(game.board()).toEqual([
+      ["X", "O", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ]);
   });
 });
--- ticTacToe/src/Game.ts
+++ ticTacToe/src/Game.ts
@@ -1,3 +1,13 @@
 export class Game {
-  
+  play(number1: number, number2: number) {
+    console.log(number1, number2);
+  }
+  board() {
+    const board = [
+      ["X", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+    return board;
+  }
 }
