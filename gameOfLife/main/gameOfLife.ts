628b6b14f589ff7b916ca188fa7382c1456a1a5b
Toby Hamand <toby.hamand@cazoo.co.uk>
2021-06-04 10:39:48 +0100

    added test for third rule

--- gameOfLife/main/gameOfLife.ts
+++ gameOfLife/main/gameOfLife.ts
@@ -6,7 +6,9 @@ export const board = (): GameOfLifeBoard => {
   ];
 };
 
-export type GameOfLifeBoard = boolean[][];
+type Cell = boolean;
+type CellCoordinates = {x: number, y: number};
+export type GameOfLifeBoard = Cell[][];
 
 export const nextGen = (board: GameOfLifeBoard) => {
   return [
@@ -16,6 +18,28 @@ export const nextGen = (board: GameOfLifeBoard) => {
   ];
 };
 
-export const checkCellItem = (cell: boolean) => {
-  console.log("blabla", cell);
+function countAliveNeighbours(board: Cell[][], x: number, y: number) {
+  const neighbour1 = board[x - 1][y - 1]
+  const neighbour2 = board[x - 1][y]
+  const neighbour3 = board[x - 1][y + 1]
+  const neighbour4 = board[x][y - 1]
+  const neighbour5 = board[x][y + 1]
+  const neighbour6 = board[x + 1][y - 1]
+  const neighbour7 = board[x + 1][y]
+  const neighbour8 = board[x + 1][y + 1]
+  return [
+    neighbour1,
+    neighbour2,
+    neighbour3,
+    neighbour4,
+    neighbour5,
+    neighbour6,
+    neighbour7,
+    neighbour8
+  ].filter((value => value)).length;
+}
+
+export const checkCellItem = ({x, y}: CellCoordinates, board: GameOfLifeBoard) => {
+  const aliveNeighbours = countAliveNeighbours(board, x, y);
+  return aliveNeighbours >= 2;
 };
--- gameOfLife/test/gameOfLife.test.ts
+++ gameOfLife/test/gameOfLife.test.ts
@@ -15,45 +15,44 @@ describe("game of life", () => {
     expect(board()).toEqual(gridBoard);
   });
 
-  describe("cell", () => {
-    it("should die if it has no neighbour", () => {
-      const board = [
-        [false, false, false],
-        [false, true, false],
-        [false, false, false],
-      ];
-      const newBoard = nextGen(board);
-      expect(newBoard).toEqual([
-        [false, false, false],
-        [false, false, false],
-        [false, false, false],
-      ]);
-    });
-
-    it("should die if it has 1 neighbour", () => {
-      const board = [
-        [false, false, false],
-        [true, true, false],
-        [false, false, false],
-      ];
-      const newBoard = nextGen(board);
-      expect(newBoard).toEqual([
-        [false, false, false],
-        [false, false, false],
-        [false, false, false],
-      ]);
-    });
-  });
-
   describe("check cell item", () => {
     it("return true if has at least 2 neighbors", () => {
-      const board = [
+      const board: GameOfLifeBoard = [
         [false, false, false],
         [true, true, false],
         [false, true, false],
       ];
 
-      expect(checkCellItem(board[1][1])).toBe(true);
+      expect(checkCellItem({x: 1, y:1}, board)).toBe(true);
+    });
+
+    describe('when a cell is alive', () => {
+      it("should return false if it has 1 neighbour", () => {
+        const board = [
+          [false, false, false],
+          [true, true, false],
+          [false, false, false],
+        ];
+        expect(checkCellItem({x: 1, y:1}, board)).toBe(false);
+      });
+
+      it("should return false if it has no neighbours", () => {
+        const board = [
+          [false, false, false],
+          [false, true, false],
+          [false, false, false],
+        ];
+        expect(checkCellItem({x: 1, y:1}, board)).toBe(false);
+      });
+
+      it("should return false if it has more than 3 neighbours", () => {
+        const board = [
+          [false, true, false],
+          [true, true, true],
+          [false, true, false],
+        ];
+        expect(checkCellItem({x: 1, y:1}, board)).toBe(false);
+      });
     });
   });
 });
