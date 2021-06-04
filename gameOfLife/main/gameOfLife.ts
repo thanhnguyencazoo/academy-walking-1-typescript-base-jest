851932fec124efd84dd38793edd3a2f0a3d16573
thanhnguyencazoo <thanh.nguyen@cazoo.co.uk>
2021-06-04 10:21:36 +0100

    test for checkCellItem

--- gameOfLife/main/gameOfLife.ts
+++ gameOfLife/main/gameOfLife.ts
@@ -3,11 +3,19 @@ export const board = (): GameOfLifeBoard => {
     [true, true, true],
     [false, false, false],
     [false, false, false],
-  ]
+  ];
 };
 
-export type GameOfLifeBoard = boolean[][]
+export type GameOfLifeBoard = boolean[][];
 
-export function nextGen(board: GameOfLifeBoard) {
-  return board
-}
+export const nextGen = (board: GameOfLifeBoard) => {
+  return [
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+  ];
+};
+
+export const checkCellItem = (cell: boolean) => {
+  console.log("blabla", cell);
+};
--- gameOfLife/test/gameOfLife.test.ts
+++ gameOfLife/test/gameOfLife.test.ts
@@ -1,4 +1,9 @@
-import { board, GameOfLifeBoard, nextGen } from "../main/gameOfLife";
+import {
+  board,
+  GameOfLifeBoard,
+  nextGen,
+  checkCellItem,
+} from "../main/gameOfLife";
 
 describe("game of life", () => {
   it("create the board", () => {
@@ -10,19 +15,45 @@ describe("game of life", () => {
     expect(board()).toEqual(gridBoard);
   });
 
-  describe('cell', () => {
-    it('should die if it has no neighbours', () => {
+  describe("cell", () => {
+    it("should die if it has no neighbour", () => {
       const board = [
         [false, false, false],
         [false, true, false],
         [false, false, false],
       ];
-      const newBoard = nextGen(board)
+      const newBoard = nextGen(board);
       expect(newBoard).toEqual([
         [false, false, false],
         [false, false, false],
         [false, false, false],
-      ])
+      ]);
+    });
+
+    it("should die if it has 1 neighbour", () => {
+      const board = [
+        [false, false, false],
+        [true, true, false],
+        [false, false, false],
+      ];
+      const newBoard = nextGen(board);
+      expect(newBoard).toEqual([
+        [false, false, false],
+        [false, false, false],
+        [false, false, false],
+      ]);
+    });
+  });
+
+  describe("check cell item", () => {
+    it("return true if has at least 2 neighbors", () => {
+      const board = [
+        [false, false, false],
+        [true, true, false],
+        [false, true, false],
+      ];
+
+      expect(checkCellItem(board[1][1])).toBe(true);
     });
   });
 });
